cbf1241ba2a95d26b07353b899a4dab5b57ab827
S M Shahinul Islam <s.m.shahinul.islam@gmail.com>
2022-10-18 12:32:19 -0400

    Solved Leetcode Problem 0295

---
+++ 0295/solution.py
@@ -0,0 +1,22 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.max_heap = []
+        self.min_heap = []
+
+    def addNum(self, num: int) -> None:
+        if len(self.min_heap) == len(self.max_heap):
+            heapq.heappush(self.min_heap, -heapq.heappushpop(self.max_heap, -num))
+        else:
+            heapq.heappush(self.max_heap, -heapq.heappushpop(self.min_heap, num))
+
+    def findMedian(self) -> float:
+        if len(self.min_heap) == len(self.max_heap):
+            return float(self.min_heap[0] - self.max_heap[0]) / 2.0
+        else:
+            return float(self.min_heap[0])
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
