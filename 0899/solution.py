622e3ea85855eba10c13b9f7e605a94c6d0fe258
S M Shahinul Islam <s.m.shahinul.islam@gmail.com>
2022-10-20 13:56:29 -0400

    Solved Leetcode Problem 0899

---
+++ 0899/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def orderlyQueue(self, s: str, k: int) -> str:
+        if k == 1:
+            return min(s[i:] + s[:i] for i in range(len(s))) # whole rotate
+        else:
+            return ''.join(sorted(s))
