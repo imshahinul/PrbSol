71ade9dc2333b9c3f5bc5cc1c7d74cb58eb39760
S M Shahinul Islam <s.m.shahinul.islam@gmail.com>
2022-12-23 06:14:23 -0500

    Better Solution for 354

--- 0354/solution.py
+++ 0354/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
         envelopes.sort(key=lambda k: (k[0], -k[1]))
-        c = 0
+        out = 0
         buckets = [None] * len(envelopes)
-        for x, y in envelopes:
+        for _, y in envelopes:
             i = 0
-            j = c
+            j = out
             while i < j:
                 k = (i + j) // 2
                 if buckets[k] >= y:
@@ -13,6 +13,6 @@ class Solution:
                 else:
                     i = k + 1
             buckets[i] = y
-            if i == c:
-                c += 1
-        return c
+            if i == out:
+                out += 1
+        return out
