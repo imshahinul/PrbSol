cad0ce402f88f634d2fc25085f6d1d49746402db
S M Shahinul Islam <s.m.shahinul.islam@gmail.com>
2023-02-06 15:13:48 -0500

    Solved Leetcode Problem 316

---
+++ 0316/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        ms, traversed = [], set()
+        cntr = {ch: i for i, ch in enumerate(s)}
+        for i, ch in enumerate(s):
+            if ch in traversed:
+                continue
+            while ms and ms[-1] > ch and cntr[ms[-1]] > i:
+                traversed.remove(ms.pop())
+            ms.append(ch)
+            traversed.add(ch)
+        return ''.join(ms)
