35db47ecb68ffc17f41bbc18b5e12bee5e97b378
S M Shahinul Islam <s.m.shahinul.islam@gmail.com>
2023-04-19 01:04:03 -0400

    Solved Leetocde Problem 1372

---
+++ Leetcode/1372/solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestZigZag(self, root: TreeNode) -> int:
+        def sub(root, lb, ub):
+            if root is None:
+                return
+            nonlocal out
+            out = max(out, lb, ub)
+            sub(root.left, ub + 1, 0)
+            sub(root.right, 0, lb + 1)
+
+        out = 0
+        sub(root, 0, 0)
+        return out
