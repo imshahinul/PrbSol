b5aaa0f255186aa62f54125a0209b35d582412ea
S M Shahinul Islam <s.m.shahinul.islam@gmail.com>
2023-05-02 13:13:47 +0600

    Solved Leetocde Problem 1822

---
+++ Leetcode/1822/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        flag = 1
+        for num in nums:
+            if num > 0:
+                flag *= 1
+            elif num < 0:
+                flag *= -1
+            else:
+                return 0
+        return flag
